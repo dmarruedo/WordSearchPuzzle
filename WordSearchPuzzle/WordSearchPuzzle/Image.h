005f42d185d9578cbc2e8e401acde711daad5578
David Marruedo Enguita <100445788@alumnos.uc3m.es>
2020-11-17 12:09:30 +0100

    codigo vacio

--- WordSearchPuzzle/WordSearchPuzzle/Image.h
+++ WordSearchPuzzle/WordSearchPuzzle/Image.h
@@ -16,17 +16,5 @@
 using namespace std;
 using namespace cv;
 
-// ratio case/number in a case
-const int case_size = matching_size * 1.6;
-
-// increase case/number size while cropping a case/number
-const int crop_size = matching_size * 0.3;
-
-int displayImage(const string display_name, Mat img);
-Mat processInput(Mat img);
-void transformScrabble(Mat img_src, Mat& img_tr, Rect& big_rect);
-void segmentSudoku(Mat img, Mat& img_num, int x, int y);
-void drawSudoku(Mat img, Rect rect, int grid[9][9]);
-
 
 #endif /* IMAGE_H_ */
--- WordSearchPuzzle/WordSearchPuzzle/OCR.h
+++ WordSearchPuzzle/WordSearchPuzzle/OCR.h
@@ -16,20 +16,3 @@ using namespace std;
 using namespace cv;
 using namespace cv::ml;
 
-// minimum height in pix for input of train data
-const int train_height_min = 9;
-
-// matching OCR pixel size (square)
-const int matching_size = 35;
-
-// minimum matching distance to solve a number
-const int minimum_matching_dist = 10000000;
-
-void trainOCR(string filename, Mat& sample, Mat& response);
-void loadTrainedOCR(string filename, Mat& sample, Mat& response);
-void saveTrainedOCR(string filename, Mat& sample, Mat& response);
-int getNumberOCR(Mat img, Ptr<KNearest> knn);
-
-
-#endif /* OCR_H_ */
-
